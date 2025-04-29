58b11bb3d412c260fca2b80fe5fe561116bebaf3
Abderrahhmane Metnani <abdulrahman.metnani@gmail.com>
2025-04-29 23:00:39 +0400

    Create constants.js

---
+++ app/containers/RegistrationPage/constants.js
@@ -0,0 +1,21 @@
+export const OPERATOR_TYPES = [
+  { value: 0, label: 'NA' },
+  { value: 1, label: 'LUC' },
+  { value: 2, label: 'Non-LUC' },
+  { value: 3, label: 'AUTH' },
+  { value: 4, label: 'DEC' },
+];
+
+export const COUNTRIES = [
+  { code: 'AE', name: 'United Arab Emirates' },
+  // Add more countries as needed
+];
+
+export const INITIAL_ADDRESS = {
+  address_line_1: '',
+  address_line_2: '-',
+  address_line_3: '-',
+  city: '',
+  country: 'AE',
+  postcode: '',
+};
